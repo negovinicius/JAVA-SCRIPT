7e050da78ea55ab66dd991148af8e4f9dc9cb688
Vinicius Back Ferreira <103704757+negovinicius@users.noreply.github.com>
2023-08-07 22:52:10 -0300

    Aula de hoje

--- inicio/controle/IFELSE.IF.js
+++ inicio/controle/IFELSE.IF.js
@@ -3,7 +3,7 @@ Number.prototype.entre = function (inicio, fim) {
 }
 
 const imprimirResultado = function (nota) {
-    if (nota.entre(9, 10)){
+    if (nota.entre(9, 10)) {
     console.log('Quadro de Honra')
 } else if(nota.entre(7, 8.99)) {
     console.log('Aprovado')
