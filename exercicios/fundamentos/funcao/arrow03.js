4c70752ea5a0e8b4a8f84097e8234bfca3c5a124
Vinicius Back Ferreira <103704757+negovinicius@users.noreply.github.com>
2023-08-23 00:06:03 -0300

    aulas de hoje

---
+++ exercicios/fundamentos/funcao/arrow03.js
@@ -0,0 +1,18 @@
+let comparaComThis = function (param) {
+    console.log(this === param)
+}
+
+comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComArrow = param => console.log(this === param)
+comparaComArrow(global)
+comparaComArrow(module.exports)
+comparaComArrow(this)
+
+comparaComArrow = comparaComArrow.bind(obj)
+comparaComArrow(obj)
